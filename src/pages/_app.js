938611f77ccfb41052bccba74e62f9fdc92afb83
Akash <akash@appycodes.com>
2025-07-09 14:01:10 +0530

    added progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 import Layout from "@/components/layouts";
 import "@/styles/globals.css";
-import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NextNProgress from "nextjs-progressbar";
+import { Toaster } from "react-hot-toast";
 
 const toastStyle = {
   background: "#363636",
@@ -13,9 +14,18 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
   return (
-    <QueryClientProvider client={queryClient}>
-      <Toaster toastOptions={{ style: toastStyle }} />
-      {getLayout(<Component {...pageProps} />)}
-    </QueryClientProvider>
+    <>
+      <NextNProgress
+        color="red"
+        startPosition={0.2}
+        stopDelayMs={100}
+        height={2}
+        showOnShallow={true}
+      />
+      <QueryClientProvider client={queryClient}>
+        <Toaster toastOptions={{ style: toastStyle }} />
+        {getLayout(<Component {...pageProps} />)}
+      </QueryClientProvider>
+    </>
   );
 }
