83dc5f6466bf254080fa25f6f47629a7d84d1e65
Akash <akash@appycodes.com>
2025-07-18 10:06:35 +0530

    logic to render content for signed out and signed in users in liokedvideos & saved videos

--- src/pages/liked-videos.js
+++ src/pages/liked-videos.js
@@ -1,22 +1,46 @@
-import { Heart } from "lucide-react";
-import Link from "next/link";
+import SignedOutUI from "@/components/signed-out/LikedVideos";
+import { constants } from "@/constants";
+import { useUser } from "@/contexts/UserContext";
+import axios from "axios";
 
-const LikedVideos = () => {
-  return (
-    <div className="w-full flex flex-col justify-center items-center space-y-6">
-      <Heart size={120} />
-      <div className="flex flex-col items-center">
-        <p className="text-lg">Watch your liked videos</p>
-        <p className="text-xs mt-1">Sign in to watch your liked videos.</p>
-        <Link
-          href="/sign-in"
-          className="mt-4 text-xs font-medium px-4 py-2 bg-red-600 rounded-full hover:bg-red-700 transition-all"
-        >
-          Sign in
-        </Link>
-      </div>
-    </div>
-  );
+const LikedVideos = ({ likedVideos, error }) => {
+  const { user } = useUser();
+
+  if (!user || (error && error[0] === 401)) {
+    return <SignedOutUI />;
+  }
+
+  if (error) {
+    return <p>{error[1]}</p>;
+  }
+
+  return <div>user logged in</div>;
 };
 
 export default LikedVideos;
+
+export async function getServerSideProps({ req }) {
+  try {
+    const likedVideos = await axios.get(
+      constants.apiURL + "/user/likedVideos/user",
+      {
+        headers: {
+          Cookie: req.headers.cookie,
+        },
+        withCredentials: true,
+      }
+    );
+
+    return {
+      props: {
+        likedVideos: likedVideos.data,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        error: [error.status, error.response.data.message],
+      },
+    };
+  }
+}
--- src/pages/watch-later.js
+++ src/pages/watch-later.js
@@ -1,23 +1,46 @@
-import { ClockFading, TimerReset } from "lucide-react";
-import Link from "next/link";
-import React from "react";
+import SignedOutUI from "@/components/signed-out/SavedVideos";
+import { constants } from "@/constants";
+import { useUser } from "@/contexts/UserContext";
+import axios from "axios";
 
-const WatchLater = () => {
-  return (
-    <div className="w-full flex flex-col justify-center items-center space-y-6">
-      <ClockFading size={120} />
-      <div className="flex flex-col items-center">
-        <p className="text-lg">Watch your saved videos</p>
-        <p className="text-xs mt-1">Sign in to watch saved videos.</p>
-        <Link
-          href="/sign-in"
-          className="mt-4 text-xs font-medium px-4 py-2 bg-red-600 rounded-full hover:bg-red-700 transition-all"
-        >
-          Sign in
-        </Link>
-      </div>
-    </div>
-  );
+const WatchLater = ({ savedVideos, error }) => {
+  const { user } = useUser();
+
+  if (!user || (error && error[0] === 401)) {
+    return <SignedOutUI />;
+  }
+
+  if (error) {
+    return <p>{error[1]}</p>;
+  }
+
+  return <>User Logged in</>;
 };
 
 export default WatchLater;
+
+export async function getServerSideProps({ req }) {
+  try {
+    const savedVideos = await axios.get(
+      constants.apiURL + "/user/savedVideos/user",
+      {
+        headers: {
+          Cookie: req.headers.cookie,
+        },
+        withCredentials: true,
+      }
+    );
+
+    return {
+      props: {
+        savedVideos: savedVideos.data,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        error: [error.status, error.response.data.message],
+      },
+    };
+  }
+}
