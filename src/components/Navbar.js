60ed2fa56b0179fed24765024b21dbde767fadce
Akash <akash@appycodes.com>
2025-07-21 10:06:09 +0530

    displaying user Image in top navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
+import { useUser } from "@/contexts/UserContext";
 import { Atom, CircleUserRound, UserRound } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const { user } = useUser();
+
   return (
     <div className="h-16 flex items-center justify-between">
       <Link href="/">
@@ -13,7 +17,17 @@ const Navbar = () => {
       </Link>
 
       <Link href="/account">
-        <CircleUserRound size={25} />
+        {user && user?.avatarURL ? (
+          <Image
+            src={user?.avatarURL}
+            alt="user-image"
+            width={30}
+            height={30}
+            className="rounded-full"
+          />
+        ) : (
+          <CircleUserRound size={30} />
+        )}
       </Link>
     </div>
   );
